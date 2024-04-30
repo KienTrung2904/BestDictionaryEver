f5d33f5d9d59091108531fca2c85380da1bab76f
KienTrung2904 <trungstatheros2904@gmail.com>
2024-05-01 04:51:08 +0700

    add score, questionIndex, health, change some thing in multiplechoice interface

--- src/main/java/controller/game/Exercise/Exercise/MultipleChoiceController.java
+++ src/main/java/controller/game/Exercise/Exercise/MultipleChoiceController.java
@@ -19,50 +19,17 @@ import java.util.ResourceBundle;
 import static controller.TopicWord.backend.TopicWords.DetailedTopicWord.DetailTopicWordLoad.globalFullDetailedTopicWordMap;
 
 public class MultipleChoiceController extends ExerciseController<MultipleChoice> implements Initializable {
-
-//    public void back() {
-//        super.chooseGame();
-//    }
     private String currentTopic;
-
-    public void backToChooseTopic() {
-        super.chooseTopicAndPractice();
-    }
-
-    public void chooseAnimal(MouseEvent mouseEvent) {
-        setCurrentTopic("Animal");
-    }
-    public void chooseFood(MouseEvent mouseEvent) {
-        setCurrentTopic("Food");
-    }
-    public void chooseSports(MouseEvent mouseEvent) {
-        setCurrentTopic("Sports");
-    }
-    public void chooseBody(MouseEvent mouseEvent) {
-        setCurrentTopic("Body");
-    }
-    public void chooseWeather(MouseEvent mouseEvent) {
-        setCurrentTopic("Weather");
-    }
-    public void choosePlants(MouseEvent mouseEvent) {
-        setCurrentTopic("Plants");
-    }
-    public void chooseBusiness(MouseEvent mouseEvent) {
-        setCurrentTopic("Business");
-    }
-    public void chooseFashion(MouseEvent mouseEvent) {
-        setCurrentTopic("Fashion");
-    }
-
     @FXML
     private VBox topicVbox, exerciseVbox;
     @FXML
     private Button AnimalButton, BodyButton, FoodButton, SportsButton, FashionButton, WeatherButton,
-            BusinessButton, PlantsButton, optionA, optionB, optionC, optionD;
+            BusinessButton, PlantsButton, optionA, optionB, optionC, optionD, optionChosen;
     @FXML
     private Button[] topicButton;
     @FXML
     private Button[] options;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -78,21 +45,82 @@ public class MultipleChoiceController extends ExerciseController<MultipleChoice>
                 }
             });
         }
-
-        for (Button option : options) {
-            option.setOnAction(new EventHandler<ActionEvent>() {
+        String[] optionText = {"A", "B", "C", "D"};
+        for (int index = 0; index < options.length; index++) {
+            int temp = index;
+            options[index].setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
-                    userAnswer = option.getText();
+                    optionChosen = options[temp];
+                    userAnswer = optionText[temp];
                     System.out.println("User choose: " + userAnswer);
                     resetButtonStyle();
-                    option.getStyleClass().clear();
-                    option.getStyleClass().add("choosingButton");
+                    options[temp].getStyleClass().clear();
+                    options[temp].getStyleClass().add("choosingButton");
                 }
             });
-
         }
 
+    }
+
+    private void goToExercise() {
+        setChooseTopicScreen(false);
+        setNextQuestion();
+
+    }
+
+
+    public void setNextQuestion() {
+        resetButtonStyle();
+        scoreLabel.setText("Score: " + score);
+        questionIndexLabel.setText("Question: " + (questionIndex + 1) + "/" + totalQuestions);
+        if (health ==  2) {
+            blood3.setVisible(false);
+        } else if (health == 1) {
+            blood2.setVisible(false);
+        } else if (health == 0) {
+            blood1.setVisible(false);
+            System.out.println("Game Over! Your score is: " + score);
+            backToChooseTopic();
+        }
+        resetButtonStyle();
+        MultipleChoice multipleChoice = exerciseList.get(questionIndex);
+        question.setText(multipleChoice.getQuestion());
+        question.setWrapText(true);
+        optionA.setText(multipleChoice.getOptionA());
+        optionB.setText(multipleChoice.getOptionB());
+        optionC.setText(multipleChoice.getOptionC());
+        optionD.setText(multipleChoice.getOptionD());
+        currentExercise = multipleChoice;
+        System.out.println(currentExercise);
+    }
+
+    public void clickCheck(MouseEvent mouseEvent) throws InterruptedException {
+        if (userAnswer == null) {
+            System.out.println("Please choose an answer");
+        } else {
+            checkAnswer(optionChosen);
+        }
+    }
+    public void backToChooseTopic() {
+        setChooseTopicScreen(true);
+    }
+    private void setCurrentTopic(String topic) {
+        currentTopic = topic;
+        setExerciseList(getMultipleChoiceExerciseListWithTopic(currentTopic));
+        totalQuestions = exerciseList.size();
+        System.out.println("Total questions: " + totalQuestions);
+        score = 0;
+        health = 3;
+        questionIndex = 0;
+        goToExercise();
+    }
+
+    public void resetButtonStyle() {
+        for (Button optionEx : options) {
+            optionEx.getStyleClass().clear();
+            optionEx.getStyleClass().add("bigButton");
+        }
     }
     private void setChooseTopicScreen(Boolean isTrue) {
         if (isTrue) {
@@ -103,19 +131,6 @@ public class MultipleChoiceController extends ExerciseController<MultipleChoice>
             exerciseVbox.setVisible(true);
         }
     }
-
-    private void setCurrentTopic(String topic) {
-        currentTopic = topic;
-        setExerciseList(getMultipleChoiceExerciseListWithTopic(currentTopic));
-        goToExercise();
-    }
-
-    private void goToExercise() {
-        setChooseTopicScreen(false);
-        setNextQuestion();
-
-    }
-
     public static ArrayList<MultipleChoice> getMultipleChoiceExerciseListWithTopic(String topicName) {
         ArrayList<MultipleChoice> multipleChoiceList = new ArrayList<>();
         ArrayList<DetailedTopicWord> detailedTopicWords = globalFullDetailedTopicWordMap.get(topicName);
@@ -126,42 +141,5 @@ public class MultipleChoiceController extends ExerciseController<MultipleChoice>
             }
         }
         return multipleChoiceList;
-    }
-    public void setNextQuestion() {
-        resetButtonStyle();
-        MultipleChoice multipleChoice = exerciseList.get(questionIndex);
-        question.setText(multipleChoice.getQuestion());
-        question.setWrapText(true);
-        optionA.setText(multipleChoice.getOptionA());
-        optionB.setText(multipleChoice.getOptionB());
-        optionC.setText(multipleChoice.getOptionC());
-        optionD.setText(multipleChoice.getOptionD());
-        if (multipleChoice.getCorrectAnswer().equals("A")) {
-            multipleChoice.setCorrectAnswer(multipleChoice.getOptionA());
-        } else if (multipleChoice.getCorrectAnswer().equals("B")) {
-            multipleChoice.setCorrectAnswer(multipleChoice.getOptionB());
-        } else if (multipleChoice.getCorrectAnswer().equals("D")) {
-            multipleChoice.setCorrectAnswer(multipleChoice.getOptionC());
-        } else {
-            multipleChoice.setCorrectAnswer(multipleChoice.getOptionD());
-        }
-        currentExercise = multipleChoice;
-
-        System.out.println(currentExercise);
-    }
-
-    public void clickCheck(MouseEvent mouseEvent) {
-        if (userAnswer == null) {
-            System.out.println("Please choose an answer");
-        } else {
-            checkAnswer();
-        }
-    }
-
-    public void resetButtonStyle() {
-        for (Button optionEx : options) {
-            optionEx.getStyleClass().clear();
-            optionEx.getStyleClass().add("bigButton");
-        }
     }
 }
--- src/main/java/controller/game/Exercise/Utils/ExerciseController.java
+++ src/main/java/controller/game/Exercise/Utils/ExerciseController.java
@@ -3,10 +3,13 @@ package controller.game.Exercise.Utils;
 import controller.ScreenControl;
 import controller.game.backend.Exercises.MultipleChoice.MultipleChoice;
 import controller.game.backend.Utils.Exercise;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -17,12 +20,21 @@ public abstract class ExerciseController<T extends Exercise> extends ScreenContr
         super.chooseGame();
     }
 
+    public static final int COEFFICIENT = 100;
+    protected int score;
+
+    protected int health = 3;
+
+    protected int totalQuestions;
+
     protected T currentExercise;
     protected int questionIndex;
+    @FXML
+    protected ImageView blood1, blood2, blood3;
 
     protected String userAnswer;
     @FXML
-    protected Label question;
+    protected Label question, scoreLabel, questionIndexLabel;
     protected ArrayList<T> exerciseList;
 
     public void getExerciseList() {
@@ -35,20 +47,20 @@ public abstract class ExerciseController<T extends Exercise> extends ScreenContr
 
     public abstract void setNextQuestion();
 
-    public void checkAnswer() {
-        if (currentExercise instanceof MultipleChoice) {
-
-        }
+    public void checkAnswer(Button option) throws InterruptedException {
         System.out.println("UserAnswer: " + userAnswer + " -- CorrectAnswer: " + currentExercise.getCorrectAnswer());
         if (userAnswer.equals(currentExercise.getCorrectAnswer())) {
-            System.out.println("Correct");
+            // set background color of button chosen to green in 2 seconds
+            score++;
         } else {
             System.out.println("Incorrect");
+            health--;
         }
         userAnswer = null;
         questionIndex++;
         setNextQuestion();
     }
+
 
 
 }
